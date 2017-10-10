415e2e54df11732ffa9c935020df4627fcee2b75
Omer <omerh127@gmail.com>
2017-10-10 19:56:34 +0300

    completed gradient and cost function

---
+++ linearRegCostFunction.m
@@ -0,0 +1,24 @@
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+
+
+m = length(y); 
+
+J = 0;
+grad = zeros(size(theta));
+
+
+param = 1 / (2 * m);
+
+all_h = X * theta;
+
+J = param * (((all_h - y)' * (all_h - y)) + lambda * theta(2:end, :)' * theta(2:end, :));
+
+grad = (1 / m) * (X' * (all_h - y));
+
+grad(2:end, :) = grad(2:end, :) + (lambda / m) * theta(2:end, :);
+
+
+
+grad = grad(:);
+
+end
